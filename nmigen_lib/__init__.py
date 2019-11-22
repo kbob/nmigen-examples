dbf231381d35982c0929f659adb6a0dff7436a1c
kbob <kbob@jogger-egg.com>
2019-11-21 23:01:35 -0500

    Add exports to nmigen_lib/__init__.py.

---
+++ nmigen_lib/__init__.py
@@ -0,0 +1,23 @@
+from .blinker import Blinker
+from .buzzer import Buzzer
+from .counter import Counter
+from .i2s import I2SOut
+from .mul import Mul
+from .pll import PLL
+from .timer import Timer
+from .uart import UART, UARTTx, UARTRx
+
+
+
+__all__ = [
+    'Blinker',
+    'Buzzer',
+    'Counter',
+    'I2SOut',
+    'Mul',
+    'PLL',
+    'Timer',
+    'UART',
+    'UARTTx',
+    'UARTRx',
+    ]
